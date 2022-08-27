94303fc14a0aebea13360f47931bf22eb0fd4f2f
ajkami <99001857+ajkami@users.noreply.github.com>
2022-08-26 19:19:40 -0600

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-word = input("Enter a word:")
-word_list = []
-while word != "":
-    word_list.append(word)
-    word = input("Enter a word: ")
-
-word_list.sort()
-print("Final list is: " + str(word_list))
